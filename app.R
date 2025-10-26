9c41ad23ad92158152f9c0dae9f31752e1ab4314
TomRunge95 <150838999+TomRunge95@users.noreply.github.com>
2025-10-26 15:18:50 +0100

    Fix environment variable name for Telegram bot token

--- app.R
+++ app.R
@@ -12,7 +12,7 @@ library(tibble)
 
 # Telegram Funktion
 send_telegram <- function(message) {
-  bot_token <- Sys.getenv("Telegram_BOT")
+  bot_token <- Sys.getenv("TELEGRAM_BOT")
   chat_id   <- Sys.getenv("TELEGRAM_CHAT")
   url <- paste0("https://api.telegram.org/bot", bot_token, "/sendMessage")
   POST(url, body = list(chat_id = chat_id, text = message), encode = "form")
